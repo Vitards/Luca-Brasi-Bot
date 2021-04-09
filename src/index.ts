bf88c8642684ecd9d64a4fc4a6fdebd5333449c0
dixter_gordong <dixter_gordong@gmail.com>
2021-04-09 11:23:12 -0700

    subscribe to submissions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 require('dotenv').config()
 import { InboxStream, CommentStream, SubmissionStream } from 'snoostorm'
 import { getClient } from './getClient'
+
+async function main() {
+  const client = getClient()
+
+  // subscribe to submissions feed
+  const submissions = new SubmissionStream(client, {
+    subreddit: 'Vitards',
+    limit: 10,
+    pollTime: 2000,
+  })
+  submissions.on('item', console.log)
+}
+
+main()
