b371afe490fda199d373d45d66259e1e063f7cb9
dixter_gordong <dixter_gordong@gmail.com>
2021-04-09 11:20:54 -0700

    changed env vars to have REDDIT_ prefix

--- src/getClient.ts
+++ src/getClient.ts
@@ -1,14 +1,19 @@
 import Snoowrap from 'snoowrap'
 const packageJson = require('../package.json')
 
-const { CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD } = process.env
+const {
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDDIT_USERNAME,
+  REDDIT_PASSWORD,
+} = process.env
 
 export const getClient = () => {
   return new Snoowrap({
-    userAgent: `node:${packageJson.name}:${packageJson.version} (by /u/${USERNAME})`,
+    userAgent: `node:${packageJson.name}:${packageJson.version} (by /u/${REDDIT_USERNAME})`,
     clientId: CLIENT_ID,
     clientSecret: CLIENT_SECRET,
-    username: USERNAME,
-    password: PASSWORD,
+    username: REDDIT_USERNAME,
+    password: REDDIT_PASSWORD,
   })
 }
