17d14e34b8dc969f5a2996af58ad2cba916ead2a
dixter_gordong <dixter_gordong@gmail.com>
2021-04-09 11:33:21 -0700

    added TODOs in processSubmission

--- src/processSubmission.ts
+++ src/processSubmission.ts
@@ -4,4 +4,12 @@ export const processSubmission = (submission: Submission) => {
   // submission.id is identifier for post
   // submission.selftext_html is content of post
   console.log(submission)
+
+  // TODO:
+  // 1. Check if this post has already been processed
+  // 2. Check if this post has any tickers. If present:
+  //   a. Check if market cap is below $1B
+  //   b. Check if this is a SPAC
+  //   c. If either of the conditions are true, remove post
+  // 3. Record that this post has been processed
 }
